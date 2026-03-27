d9a9aaa37c4caccd19c32347b8bf3979a1bd50bd
wlscksdjs-bit <wlscksdjs@gmail.com>
2026-03-27 11:37:43 +0900

    Auto commit on save

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'role', 'team', 'is_active']
+    list_filter = ['role', 'team', 'is_active']
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+    ordering = ['-date_joined']
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('추가 정보', {'fields': ('role', 'team', 'department', 'phone', 'position')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('추가 정보', {'fields': ('role', 'team', 'department', 'phone', 'position')}),
+    )
