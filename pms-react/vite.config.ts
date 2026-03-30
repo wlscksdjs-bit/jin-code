3d603dcc6dd76dbd8fc112b5e3e4cca1661793f4
wlscksdjs-bit <wlscksdjs@gmail.com>
2026-03-30 18:54:56 +0900

    Auto commit on save

--- pms-react/vite.config.ts
+++ pms-react/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://172.25.109.251:8000',
         changeOrigin: true,
       },
     },
